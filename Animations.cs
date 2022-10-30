e38e793a42ab4b7258fd96e90808f15af5f3808b
Александр <sasha99nenyuk@gmail.com>
2022-10-30 21:01:00 +0400

    Добавьте файлы проекта.

---
+++ Animations.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Media.Animation;
+
+namespace HM
+{
+    internal  class Animations
+    {
+        /// <summary>
+        /// Линейная анимация 
+        /// </summary>
+        /// <param name="b">Обьект</param>
+        /// <param name="marginA"> Начальнапя точка 0</param>
+        /// <param name="marginB"> Конечная точка 1</param>
+        /// <param name="t"> Время (миллисекунды)</param>
+        public void LinearAnimation(System.Windows.Controls.TabControl b, int marginA, int marginB, double t )
+        {   
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = marginA;
+            animation.To = marginB;
+            animation.Duration = TimeSpan.FromMilliseconds( t );
+          b.BeginAnimation(System.Windows.Controls.TabControl.MarginProperty, animation);           
+    
+        }
+        /// <summary>
+        ///  Плавная анимация числа 
+        /// </summary>
+        ///  <param name="s"> Ссылка на переменную! </param>
+        /// <param name="min"> Минимальное значание (ОТ) </param>
+        /// <param name="max"> Максимальное значение</param>
+        public void SwitchAnimation(ref double s, double min, double max)
+        {
+            /// 0 * 0-10
+            if (min < max)
+            { //обычное прибавление 
+                while (s + (max - s) / 8 < max)
+                {
+                    TimerZ(1);
+                    s = s + (max - s) / 8 + 2;
+                    if (s > max - 1 )
+                    {
+                        s = max;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                //отрицательное прибавление
+                while (s - s / 8 > min)
+                {
+
+                    TimerZ(1);
+                    s =s - s / 8;
+                    if (s < min + 1)
+                    {
+                        s = min;
+                        break;
+                    }
+                }
+
+            }
+
+        }
+        /// <summary>
+        ///  Таймер 
+        /// </summary>
+        /// <param name="sec"> (Миллисекунды)</param>
+        async public void TimerZ(int sec)
+        {
+            await Task.Delay(sec);
+        }
+        
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Drawing.Printing;
+using System.Threading.Tasks;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+using Brushes = System.Drawing.Brushes;
+using System.Windows.Input;
+using MessageBox = System.Windows.Forms.MessageBox;
+using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Window = System.Windows.Window;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace HM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TB.Margin = new Thickness(0, 0, 0, 0);
+           
+
+
+        }
+     //Обьявление классов / глобальных переменных        
+        Animations Animations = new Animations();
+        bool SetPanel = false; //false - закрытая панель, true - открытая панлеь
+
+#if Button_close
+        ///// <summary>
+        //////кнопка закрытия 
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //private void Close_Butt_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        //{
+
+        //}
+        ///// <summary>
+        ///// смеа цвета на красный
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //private void Close_Butt_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        //{
+        //    Close_Butt.Background = new SolidColorBrush(Colors.Red);
+        //}
+        ///// <summary>
+        ///// смена цвета на = не цвет
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //private void Close_Butt_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        //{
+
+        //    Close_Butt.Background = null;
+        //}
+#endif
+
+        /// <summary>
+        ///Кнопка боковой панели 
+        /// </summary>
+        private async void Image_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            const int marginLeftMin = 0;
+            const int marginLeftMax = 150;
+           double  CurrentVarginLeft  = TB.Margin.Left; 
+
+            ///анимация боковой панели  
+            if (SetPanel == true)
+            {
+
+               while (TB.Margin.Left - TB.Margin.Left/8 > marginLeftMin)
+                {
+                    CurrentVarginLeft = TB.Margin.Left;
+                    await Task.Delay(1);
+                        TB.Margin = new Thickness(CurrentVarginLeft - CurrentVarginLeft/8, 0, 0, 0);
+                    if (TB.Margin.Left < 1)
+                    { 
+                        TB.Margin = new Thickness(0, 0, 0, 0);
+                        break;
+                    }
+                }
+               
+                SetPanel = !SetPanel;
+            }
+            else
+            {
+                while (CurrentVarginLeft + (marginLeftMax - CurrentVarginLeft) / 8  < marginLeftMax)
+                {
+                    CurrentVarginLeft = TB.Margin.Left;
+                    await Task.Delay(1);
+                    TB.Margin = new Thickness(CurrentVarginLeft + (marginLeftMax - CurrentVarginLeft) / 8 , 0, 0, 0);
+                    if (TB.Margin.Left > 149)
+                    {
+                        TB.Margin = new Thickness(150, 0, 0, 0);
+                        break;
+                    }
+                }
+      
+                SetPanel = !SetPanel;
+                
+            }
+            
+
+
+
+        }
+
+      
+        private void TB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //TB.SelectedIndex = 1; MessageBox.Show(TB.SelectedIndex.ToString());      
+
+        }
+
+
+        private void SettingCanvas_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+           // MessageBox.Show("\"Настроечки\"");
+        }
+
+        private void ListProcess_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> str = new List<string>();
+
+            if (ContextRP.IsChecked == true)
+            {
+                //по сенарию RP (simple_List)
+                TextBox.Text = TextBox.Text.Replace("RP","");
+           
+            
+                            //###_refer_###
+                //TextBox.Text.Split('\n', StringSplitOptions.RemoveEmptyEntries)[TextBox.LineCount-1] + "," ; //отсавляю все что до символа? с указанием номера строки  - по сути сама RP
+                ////TextBox.GetLineText(3); // получаю саму строку по номеру 
+                //for (int i = 0; i < TextBox.LineCount - 1; i++) str.Add(TextBox.GetLineText(i) + ",");
+                //TextBox.Text = null;
+                //for (int i = 0; i < str.Count; i++) TextBox.Text += str[i];
+            }
+            else
+            {
+                //по сценарию с UPPER`S
+
+                TextBox.Text = TextBox.Text.Replace("\r\n", "')," + "\rUPPER ('") + "')";
+                TextBox.Text =  "UPPER ('" + TextBox.Text;
+
+            }
+
+            //______________работа с запятыми______________
+            if ( comma.IsEnabled == true && comma.IsChecked == true)
+            {
+                TextBox.Text = TextBox.Text.Replace("\r\n", ",\n"); // - работает
+            }
+            if (comma.IsEnabled == true && comma.IsChecked == false)
+            {
+                TextBox.Text = TextBox.Text.Replace(",", "\r");
+            }
+            
+
+
+            
+        }
+
+        /// <summary>
+        /// радио ботон 1 -RP
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ConrextUpper_Checked(object sender, RoutedEventArgs e)
+        {
+            comma.IsEnabled = false;
+        }
+
+        /// <summary>
+        ///Радио батон 2 - UPPER
+        /// </summary>
+
+        private void ContextRP_Checked(object sender, RoutedEventArgs e)
+        {
+            comma.IsEnabled = true;
+        }
+    }
+}
