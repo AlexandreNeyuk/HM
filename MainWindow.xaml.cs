47b78e75692d07a1b3fd6c89da8236c71ec733e8
anton566344 <tancist2012@yandex.ru>
2022-12-09 19:36:59 +0400

    исправлен баг с буфером обмена

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -303,14 +303,16 @@ namespace HM
 
                 }
             }
-
-
-
-            if (TextBox.Text != "") Clipboard.SetText(TextBox.Text); //запись в  буфер
+            try
+            {
+                if (TextBox.Text != "") Clipboard.SetText(TextBox.Text); //запись в  буфер
+            }
+            catch
+            { 
             BFcopy.Text = "Результат скопирован в буфер обмена";
             await Task.Delay(1000);
             BFcopy.Text = null;
-
+            }
         }
 
         /// <summary>
